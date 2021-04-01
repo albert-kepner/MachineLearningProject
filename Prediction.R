6d5b8f1b62616e55fc0b6b21b46a6a48e4901480
Al Kepner <albert.kepner@outlook.com>
2021-03-31 21:34:12 -0400

    basic tree prediction

--- Prediction.R
+++ Prediction.R
@@ -1,74 +1,46 @@
 library(dplyr)
 library(caret)
+library(rattle)
 
 training_raw <- read.csv("data/pml-training.csv")
 testing_raw <- read.csv("data/pml-testing.csv")
-
-names(training)
-
-names(testing)
-
-str(training)
-
-summary(training)
-
-
-na_count <-sapply(training_raw, function(y) sum(length(which(is.na(y)))))
-
-na_test <-sapply(testing_raw, function(y) sum(length(which(is.na(y)))))
-
-na_count
-
-na_count2 <-lapply(training, function(y) sum(length(which(is.na(y)))))
-
-x <- c(2,5,3,9,8,11,6)
-count <- 0
-for (val in x) {
-  if(val %% 2 == 0)  {
-    count = count+1
-    print(val)
-  }
-}
-print(count)
-
 raw <- training_raw
 
-ss <- seq_len(length(raw))
 
-for (index in seq(1,length(raw), by=1)) {
-  any_na <- any(is.na(raw[,index]))
-  msg <- paste(index, any_na)
-  print(msg)
-}
 
-raw_has_na <- function (index) {
-  any(is.na(raw[,index]))
-}
-
-raw_no_na <- function (index) {
-  !any(is.na(raw[,index]))
+all_num <- function (index) {
+  ifelse(
+    class(raw[,index]) == "integer" || class(raw[,index]) == "numeric'",
+    !is.na(sum(raw[,index])), FALSE)
 }
 
 
 
-has_na <- sapply(seq_along(raw), raw_has_na)
+data_f1 <- raw[all_nums]
 
-no_na <- sapply(seq_along(raw), raw_no_na)
-data_f1 <- raw[,no_na]
+data_f1 <- data_f1[,-seq(1,4)]
 
-data_na <- raw[,has_na]
-data_na <- data_na %>% mutate(classe=raw$classe)
-data_na2 <- data_na[!is.na(data_na$max_roll_belt),]
-
-count_na_not <- function(index) {
-  sum(!is.na(data_na[,index]))
-}
-
-not_nas <- sapply(seq_along(data_na), count_na_not)
-
-not_nas
-
-sum(no_na)
+data_f1 <- data_f1 %>% mutate(classe=raw$classe)
 
 
+## Separate testing and training data sets.
+set.seed(12345)
+inTrain <- createDataPartition(data_f2$classe, p=0.70, list=FALSE)
+training <- data_f1[inTrain,]
+testing <- data_f1[-inTrain,]
+
+
+M <- abs(cor(training[,-26]))
+diag(M) <- 0
+which(M > 0.9, arr.ind=TRUE)
+plot(training[,1],training[,3])
+
+
+## Predicting with trees
+modFit <- train(classe ~ .,method="rpart", data=training)
+
+print(modFit$finalModel)
+
+library(rattle)
+fancyRpartPlot(modFit$finalModel)
 
